14a9b45ef5f7b94e2b8789ee41baced148d10a8e
Ivan Rudoy - Work <rudooy@gmail.com>
2014-06-15 22:33:25 +1100

    sliders done

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,115 @@
 
+///////////////////
+//  SPEC-SLIDER  //
+///////////////////
+
+
 (function(){
+
+    var easing = "easeInOutSine";
+    $('.specslider__nav').on( 'click', function( event ) {
+        //debugger;
+        var $this = $(this);
+        var $inner = $this.parent().parent().find('.specslider__wrapper');
+        var maxCount = $inner.find('.specslider__item').length-4;
+        if ($inner.attr('data-count') == undefined)
+            $inner.attr('data-count', 0);
+        var count = parseInt($inner.attr('data-count'));
+        var marg = parseInt($inner.css('margin-left'));
+        var width = parseInt($inner.find('.specslider__item').css('width'));
+
+        event.preventDefault();
+        if ($inner.is(':animated')) {return;}
+        
+        if ( $this.hasClass("specslider__nav--prev") ) {
+            if (count <= 0) {
+                return;
+            } else {
+                marg = marg+width;
+                count -= 1;
+            }
+        } else if (count < maxCount) {
+            marg = marg-width;
+            count += 1;
+        }
+
+        $inner.animate({
+            marginLeft: marg+'px'
+        }, {
+          duration: 500,
+          easing: easing
+        });
+
+        $inner.attr('data-count', count);
+        
+    });
+})();
+
+/////////////////
+//    SLIDER   //
+/////////////////
+
+(function(){
+
+    var sliderSpeed = 500;
+
+    var slideCount = $('#slider-hero ul.slider__container li.slider__item').length;
+
+    $('#slider-hero ul.slider__container li.slider__item').each(function(i, el){
+        $(el).attr('data-index', i);
+        $('#slider-hero .slider__dots').append('<div class="slider__dot"></div>');
+    });
+    $('#slider-hero .slider__dot:eq(0)').addClass('active');
+
+    if ($('.slider__item').length > 0) {
+        if ($('#slider-hero').attr('data-count') == undefined)
+            $('#slider-hero').attr('data-count', 0);
+        var count = parseInt($('#slider-hero').attr('data-count'));
+
+        var slidesCount = $('#slider-hero .slider__item').size()-1;
+    }
+
+    $('#slider-hero .slider__nav').on('click', function(e) {
+        e.preventDefault();
+        if ($('#slider-hero .slider__item:animated').size()>0) return;
+        
+        var direction;
+        $(this).hasClass('slider__nav--next') ? direction = 1 : direction = 0;
+
+        if (direction == 0)
+            (count == 0) ? count = slidesCount + 1 : count = count;
+        else
+            (count == slidesCount) ? count = - 1 : count = count;
+
+        $('#slider-hero .slider__item.active').fadeOut(sliderSpeed, 'easeInOutQuad', function() {
+            $(this).removeClass('active');
+            (direction == 1) ? count++ : count--;
+            $('#slider-hero .slider__item').eq(count).hide();
+            $('#slider-hero .slider__item').eq(count).addClass('active');
+            $('#slider-hero .slider__item').eq(count).fadeIn(sliderSpeed, 'easeInOutQuad');
+            $('#slider-hero .slider__dot.active').removeClass('active');
+            $('#slider-hero .slider__dot').eq(count).addClass('active');
+            ///
+            $('#slider-hero').attr('data-count', count);
+        });
+    });
+
+})();
+
+
+////////////////////////
+//  PLACEHOLDERS FIX  //
+////////////////////////
+
+(function(){
+
+    if ($.fn.placeholder.input && $.fn.placeholder.textarea) {
+    } else if ($.fn.placeholder.input) {
+        $('textarea').placeholder();
+    } else {
+        $('input, textarea').placeholder();
+    }
+
 })();
 
 	/////////////////
@@ -59,65 +169,6 @@
 // 		});
 
 // 	})();	
-
-// 	////////////////////////
-// 	//  PLACEHOLDERS FIX  //
-// 	////////////////////////
-
-// 	if ($.fn.placeholder.input && $.fn.placeholder.textarea) {
-// 	} else if ($.fn.placeholder.input) {
-// 		$('textarea').placeholder();
-// 	} else {
-// 		$('input, textarea').placeholder();
-// 	}
-
-// 	///////////////////
-// 	//  SPEC-SLIDER  //
-// 	///////////////////
-
-
-// 	(function(){
-
-// 		var easing = "easeInOutSine";
-// 		$('.specslider__nav a').on( 'click', function( event ) {
-// 			//debugger;
-// 			var $this = $(this);
-// 			var $inner = $this.parent().parent().find('.specslider__wrapper');
-// 			var maxCount = $inner.find('.specslider__item').length-4;
-// 			if ($inner.attr('data-count') == undefined)
-// 				$inner.attr('data-count', 0);
-// 			var count = parseInt($inner.attr('data-count'));
-// 			var marg = parseInt($inner.css('margin-left'));
-// 			var width = parseInt($inner.find('.specslider__item').css('width'));
-
-// 			event.preventDefault();
-// 			if ($inner.is(':animated')) {return;}
-			
-// 		    if ( $this.hasClass("specslider__nav--prev") ) {
-// 		    	if (count <= 0) {
-// 		    		return;
-// 		    	} else {
-// 		    		marg = marg+width;
-// 		    		count -= 1;
-// 		    	}
-// 		    } else if (count < maxCount) {
-// 				marg = marg-width;
-// 				count += 1;
-// 			}
-
-// 			$inner.animate({
-// 				marginLeft: marg+'px'
-// 			}, {
-// 			  duration: 500,
-// 			  easing: easing
-// 			});
-
-// 			$inner.attr('data-count', count);
-			
-// 		});
-// 	})();
-
-// })(jQuery);	
 
 // 	////////////////////////
 // 	//  FORMS VALIDATION  //
