7cc3b14bf126f7b632c3f0ab1a6b62ce03a8705c
Ivan Rudoy - Work <rudooy@gmail.com>
2014-06-19 02:19:40 +1100

    added modal

--- dest/js/app.js
+++ dest/js/app.js
@@ -1,4 +1,20 @@
 
+$.modal.defaults = {
+  overlay: "#DCDCDC",        // Overlay color
+  opacity: 0.8,          // Overlay opacity
+  zIndex: 100,              // Overlay z-index.
+  escapeClose: true,      // Allows the user to close the modal by pressing `ESC`
+  clickClose: true,       // Allows the user to close the modal by clicking the overlay
+  closeText: 'Закрыть',     // Text content for the close <a> tag.
+  closeClass: '',         // Add additional class(es) to the close <a> tag.
+  showClose: true,        // Shows a (X) icon/link in the top-right corner
+  modalClass: "modal",    // CSS class added to the element being displayed in the modal.
+  spinnerHtml: null,      // HTML appended to the default spinner during AJAX requests.
+  showSpinner: true,      // Enable/disable the default spinner during AJAX requests.
+  fadeDuration: null,     // Number of milliseconds the fade transition takes (null means no transition)
+  fadeDelay: 1.0          // Point during the overlay's fade-in that the modal begins to fade in (.5 = 50%, 1.5 = 150%, etc.)
+};
+
 /////////////////
 //    SLIDER   //
 /////////////////
